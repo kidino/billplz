0d312f327dc21c34f6ecdbaa907e49d87134cbc0
Iszuddin Ismail <dino@websegera.my>
2016-01-02 17:12:55 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
-# billplz
-PHP library for working with BillPlz API
+# PHP BillPlz
+
+This is a simple library for working with Malaysia's BillPlz online payment. Be sure that you read the BillPlz API Documentation and understand what it offers before using this library.
+
+https://billplz.com/api
+
+## How to use
+
+### Create New Collection
+
+Be sure that you save the result. Result you get the is normal Curl result.
+
+```php
+use Kidino\Billplz;
+
+$bplz = new Billplz(array('api_key' => 'your api key'));
+$bplz->set_data('title','Home Tutoring');
+$bplz->set_data('logo','/xampp/htdocs/billplz/logo.png');
+$result = $bplz->create_collection();
+list($rheader, $rbody) = explode("\n\n", $result);
+$bplz_result = json_decode($rbody);
+```
+
+### Create a New Bill
+
+Please note that you can also use `set_data()` with an array instead of two parameters.
+
+```php
+use Kidino\Billplz;
+
+$bplz = new Billplz(array('api_key' => 'your api key'));
+$bplz->set_data(array(
+	'collection_id' => 'your collection id',
+	'email' => 'customer@email.com',
+	'mobile' => '60123456789',
+	'name' => "Jone Doe",
+	'due_at' => "2016-1-1",
+	'amount' => 2000, // RM20
+	'callback_url' => "http://yourwebsite.com/return_url"
+));
+
+$result = $bplz->create_bill();
+list($rheader, $rbody) = explode("\n\n", $result);
+$bplz_result = json_decode($rbody);
+```
+
+### Get Bill Details
+
+```php
+use Kidino\Billplz;
+
+$bplz = new Billplz(array('api_key' => 'your api key'));
+$result = $bplz->get_bill( 'your bill id' );
+list($rheader, $rbody) = explode("\n\n", $result);
+$bplz_result = json_decode($rbody);
+```
+
+### Delete Bill
+
+```php
+use Kidino\Billplz;
+
+$bplz = new Billplz(array('api_key' => 'your api key'));
+$result = $bplz->delete_bill( 'your bill id' );
+list($rheader, $rbody) = explode("\n\n", $result);
+$bplz_result = json_decode($rbody);
+```
---
+++ src/Kidino/Billplz/Billplz.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Kidino\Billplz;
+
+class Billplz {
+
+	private $host = 'https://www.billplz.com/api/v2';
+	private $data = array();
+	private $api_key = '';
+	private $ch;
+	private $sep = '/';
+
+	private $end_bills = 'bills';
+	private $end_collections = 'collections';
+
+	function __construct( $data = array() ){
+		if (is_array($data) && (count($data) > 0)) {
+			if (isset($data['api_key'])) $this->api_key = $data['api_key'];
+			if (isset($data['host'])) $this->host = $data['host'];
+		}
+	}
+
+	function create_collection(){
+		$this->ch = curl_init($this->host . $this->sep . $this->end_collections);
+
+		if (isset($this->data['logo'])) {
+			if (file_exists($this->data['logo'])) {
+				$this->data['logo'] = '@'. $this->data['logo'];
+			} else {
+				$this->error = "logo file not found";
+				return false;
+			}
+		}
+
+		return $this->_run();
+	}
+
+	function create_bill(){
+		$this->ch = curl_init($this->host . $this->sep . $this->end_bills);
+		return $this->_run();
+	}
+
+	function get_bill( $bill_id ){
+		$this->ch = curl_init($this->host . $this->sep . $this->end_bills . $this->sep . $bill_id);
+		return $this->_run();
+		return false;
+	}
+
+	function delete_bill( $bill_id ){
+		$this->ch = curl_init($this->host . $this->sep . $this->end_bills . $this->sep . $bill_id);
+		curl_setopt($this->ch, CURLOPT_CUSTOMREQUEST, "DELETE");
+		return $this->_run();
+	}
+
+	function set_data($data, $data2 = null) {
+		if (is_array($data)) {
+			foreach($data as $key => $value){
+				$this->data[$key] = $value;
+			}
+		} else if ($data2 !== null) {
+			$this->data[$data] = $data2;
+		}
+	}
+
+	function _run(){
+		curl_setopt($this->ch, CURLOPT_HEADER, 1);
+		curl_setopt($this->ch, CURLOPT_USERPWD, $this->api_key . ":");
+		curl_setopt($this->ch, CURLOPT_TIMEOUT, 30);
+		curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, TRUE);
+
+		if (count($this->data) > 0) {
+			curl_setopt($this->ch, CURLOPT_POSTFIELDS, $this->data );
+		}
+
+		$r = curl_exec($this->ch);
+		curl_close($this->ch);
+		return $r;
+	}
+}
